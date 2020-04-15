963330163cb6c35f17dee7b4e3fc20f9f31c1d98
IroncrossBG <m.tsolevski@gmail.com>
2020-04-15 13:17:26 +0300

    Added Weather View component

---
+++ NewsSite/Models/View/WeatherViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsSite.Models.View
+{
+    public class WeatherViewModel
+    {
+        public string Condition { get; set; }
+        public double Temperature { get; set; }
+        public double MinTemperature { get; set; }
+        public double MaxTemperature { get; set; }
+        public double FeelsLikeTemperature { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+        public double WindSpeed { get; set; }
+    }
+}
---
+++ NewsSite/ViewComponents/WeatherViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsSite.Models.View;
+using NewsSite.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsSite.ViewComponents
+{
+    [ViewComponent(Name = "Weather")]
+    public class WeatherViewComponent : ViewComponent
+    {
+        private readonly IWeatherService weatherService;
+
+        public WeatherViewComponent(IWeatherService categoryService)
+        {
+            this.weatherService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var weather = weatherService.GetWeatherData("Sofia", "");
+            var result = weather.Result;
+            var resultModel = new WeatherViewModel()
+            {
+                Condition = result.weather.FirstOrDefault().main,
+                Temperature = result.main.temp,
+                MinTemperature = result.main.temp_min,
+                MaxTemperature = result.main.temp_max,
+                FeelsLikeTemperature = result.main.feels_like,
+                Pressure = result.main.pressure,
+                Humidity = result.main.humidity,
+                WindSpeed = result.wind.speed
+            };
+
+            return View(resultModel);
+        }
+    }
+}
